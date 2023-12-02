e2bc19aaec6071d8ef908a739e967cf6e1ec2374
wdavid <wdavid>
2023-12-02 13:49:12 -0600

    deploy

---
+++ Backend/src/Libraries/Cloudinary.js
@@ -0,0 +1,16 @@
+import cloudinary from 'cloudinary';
+import { ApiKey, ApiSecret, CloudName } from '../SettingEnv.js';
+
+cloudinary.v2.config({
+  api_key: ApiKey,
+  api_secret: ApiSecret,
+  cloud_name: CloudName
+})
+
+export async function UploadImage(image) {
+  return await cloudinary.v2.uploader.upload(image, { folder: 'Denunicas' })
+}
+
+export async function DeleteImage(id) {
+  return await cloudinary.v2.uploader.destroy(id);
+}
